6e2191bbca823589c96226607fc26e607e738c12
Koposov Seryozha <106330738+koposovseryozha@users.noreply.github.com>
2026-01-28 16:05:24 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -27,10 +27,14 @@ let package = Package(
         products: [
             .library(name: "Starscream", targets: ["Starscream"])
         ],
-        dependencies: [
-          .package(url: "https://github.com/apple/swift-nio-zlib-support.git", from: "1.0.0")
-        ],
+        dependencies: [],
         targets: [
-            .target(name: "Starscream")
+            .target(name: "Starscream",
+                    path: "Sources",
+                    resources: [.copy("PrivacyInfo.xcprivacy")])
         ]
 )
+
+#if os(Linux)
+    package.dependencies.append(.package(url: "https://github.com/apple/swift-nio-zlib-support.git", from: "1.0.0"))
+#endif
